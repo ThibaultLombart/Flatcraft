7a6092226b1b5519b5bfb6add1636aa9dd708a3c
Brayan Bouzrou <bouzroubrayan@gmail.com>
2023-10-06 15:33:06 +0200

    ✨ 2)a)Création de la classe GenerateCell et définition des constructeurs de la classe parente

---
+++ src/main/java/fr/univartois/butinfo/r304/flatcraft/model/map/GenerateCell.java
@@ -0,0 +1,36 @@
+package fr.univartois.butinfo.r304.flatcraft.model.map;
+
+import fr.univartois.butinfo.r304.flatcraft.model.IMovable;
+import fr.univartois.butinfo.r304.flatcraft.model.resources.Resource;
+import fr.univartois.butinfo.r304.flatcraft.view.Sprite;
+
+public class GenerateCell extends AbstractCell{
+
+	public GenerateCell(int row, int column) {
+		super(row, column);
+		// TODO Auto-generated constructor stub
+	}
+
+	public GenerateCell(Resource resource) {
+		super(resource);
+		// TODO Auto-generated constructor stub
+	}
+
+	public GenerateCell(Sprite sprite) {
+		super(sprite);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean move(IMovable movable) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean dig(IMovable player) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+}
