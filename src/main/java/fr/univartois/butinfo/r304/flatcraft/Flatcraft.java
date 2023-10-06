e71f09befc707a3df2a7d2f16dbe668a3e879efc
Brayan Bouzrou <bouzroubrayan@gmail.com>
2023-10-06 17:17:35 +0200

    ✨ 2)e)Modification de la classe FlatCraft avec l'ajout de ChooseSprite

--- src/main/java/fr/univartois/butinfo/r304/flatcraft/Flatcraft.java
+++ src/main/java/fr/univartois/butinfo/r304/flatcraft/Flatcraft.java
@@ -19,6 +19,7 @@ package fr.univartois.butinfo.r304.flatcraft;
 import java.io.IOException;
 
 import fr.univartois.butinfo.r304.flatcraft.controller.FlatcraftController;
+import fr.univartois.butinfo.r304.flatcraft.model.ChooseSprite;
 import fr.univartois.butinfo.r304.flatcraft.model.FlatcraftGame;
 import fr.univartois.butinfo.r304.flatcraft.view.SpriteStore;
 import javafx.application.Application;
@@ -61,7 +62,7 @@ public final class Flatcraft extends Application {
 
         // On crée ensuite le jeu, que l'on lie au contrôleur.
         // TODO Utiliser ici la bonne factory pour créer les objets du jeu.
-        FlatcraftGame game = new FlatcraftGame(GAME_WIDTH, GAME_HEIGHT, new SpriteStore(), null);
+        FlatcraftGame game = new FlatcraftGame(GAME_WIDTH, GAME_HEIGHT, new SpriteStore(), new ChooseSprite());
         controller.setGame(game);
         game.setController(controller);
         game.prepare();
