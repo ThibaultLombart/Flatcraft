4fcf226db745df1275cbfb49334ef1bcac74b68d
Brayan Bouzrou <bouzroubrayan@gmail.com>
2023-10-06 17:13:08 +0200

    ✨ 2)d)Création de la classe ChooseSprite et implémentation des constructeurs AbstractCell

---
+++ src/main/java/fr/univartois/butinfo/r304/flatcraft/model/ChooseSprite.java
@@ -0,0 +1,73 @@
+package fr.univartois.butinfo.r304.flatcraft.model;
+
+import java.util.Random;
+
+import fr.univartois.butinfo.r304.flatcraft.model.map.GenerateCell;
+import fr.univartois.butinfo.r304.flatcraft.view.ISpriteStore;
+
+public class ChooseSprite implements CellFactory {
+	
+	private ISpriteStore spriteStore;
+	
+	@Override
+	public void setSpriteStore(ISpriteStore spriteStore) {
+		// TODO Auto-generated method stub
+		this.spriteStore = spriteStore;
+	}
+
+	@Override
+	public Cell createSky() {
+		// TODO Auto-generated method stub
+		Random r = new Random();
+        int chance = r.nextInt(4);
+        if (chance <= 3) {
+        	return new GenerateCell(this.spriteStore.getSprite("default_ice"));
+		}
+		return new GenerateCell(this.spriteStore.getSprite("default_cloud"));
+	}
+
+	@Override
+	public Cell createSoilSurface() {
+		// TODO Auto-generated method stub
+		Random r = new Random();
+        int chance = r.nextInt(4);
+        if (chance <= 3) {
+        	return new GenerateCell(this.spriteStore.getSprite("default_grass"));
+		}
+		return new GenerateCell(this.spriteStore.getSprite("default_water"));
+	}
+	@Override
+	public Cell createSubSoil() {
+		// TODO Auto-generated method stub
+		return new GenerateCell(this.spriteStore.getSprite("default_dirt"));
+	}
+
+	@Override
+	public Cell createTrunk() {
+		// TODO Auto-generated method stub
+		Random r = new Random();
+        int chance = r.nextInt(8);
+        if (chance <= 4) {
+        	return new GenerateCell(this.spriteStore.getSprite("default_tree"));
+		}
+        if (chance <= 6 && chance > 4) {
+        	return new GenerateCell(this.spriteStore.getSprite("default_acasia_tree"));
+		}
+		return new GenerateCell(this.spriteStore.getSprite("default_aspen_tree"));
+	}
+
+	@Override
+	public Cell createLeaves() {
+		// TODO Auto-generated method stub
+		Random r = new Random();
+        int chance = r.nextInt(8);
+        if (chance <= 4) {
+        	return new GenerateCell(this.spriteStore.getSprite("default_leaves"));
+		}
+        if (chance <= 6 && chance > 4) {
+        	return new GenerateCell(this.spriteStore.getSprite("default_acasia_leaves"));
+		}
+		return new GenerateCell(this.spriteStore.getSprite("default_aspen_leaves"));
+	}
+
+}
