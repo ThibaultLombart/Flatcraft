92bc1d83e620f8f802950e30e8c5ad40994c4e28
Lucas Debloos <deblooslucas@gmail.com>
2023-10-06 15:40:42 +0200

    ✨ b. Player.java

--- src/main/java/fr/univartois/butinfo/r304/flatcraft/model/Player.java
+++ src/main/java/fr/univartois/butinfo/r304/flatcraft/model/Player.java
@@ -13,7 +13,39 @@ public class Player {
 	
 	private ObservableMap<Resource, Integer> inventory = FXCollections.observableHashMap();
 
+	public int getHealthPoints() {
+		return healthPoints.getValue();
+	}
+
+	public void setHealthPoints(IntegerProperty healthPoints) {
+		this.healthPoints.setValue(healthPoints.getValue());
+	}
+
+	public int getXpPoints() {
+		return xpPoints.getValue();
+	}
+
+	public void setXpPoints(IntegerProperty xpPoints) {
+		this.xpPoints.setValue(xpPoints.getValue());
+	}
+
+	public void addItem(Resource res) {
+		if (this.inventory.containsKey(res)) {
+			this.inventory.put(res, this.inventory.get(res)+1);
+		}
+		else {
+			this.inventory.put(res, 1);
+		}
+	}
 	
+	public void removeItem(Resource res) {
+		if (this.inventory.containsKey(res)) {
+			if (this.inventory.get(res)==1) {
+				this.inventory.remove(res);
+			}
+			this.inventory.put(res, this.inventory.get(res)-1);
+		}
+	}
 	
 	
 	
