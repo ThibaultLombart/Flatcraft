7ac7e0af2dab1e30f88db4fac47a7464ebe7f6d7
Thibault Lombart <lombart.thibault@gmail.com>
2023-10-06 15:21:35 +0200

    ✨ A. creation MapGenerator.java

---
+++ src/main/java/fr/univartois/butinfo/r304/flatcraft/model/map/MapGenerator.java
@@ -0,0 +1,27 @@
+/**
+ * Ce fichier fait partie du projet flatcraft.
+ *
+ * (c) 2023 thibault.lombart
+ * Tous droits réservés.
+ */
+
+package fr.univartois.butinfo.r304.flatcraft.model.map;
+
+
+/**
+ * Le type MapGenerator
+ *
+ * @author thibault.lombart
+ *
+ * @version 0.1.0
+ */
+public class MapGenerator {
+    
+    public static SimpleGameMap createMapGen(int hauteur, int largeur, CellFactory cellFactory) {
+        
+        SimpleGameMap map = new SimpleGameMap();
+        
+    }
+
+}
+
