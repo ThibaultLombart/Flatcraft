c307536f73e34168b89f554d59bd26748dd281ab
Thibault Lombart <lombart.thibault@gmail.com>
2023-10-20 17:26:44 +0200

    classe Mob

---
+++ src/main/java/fr/univartois/butinfo/r304/flatcraft/model/movables/Mob.java
@@ -0,0 +1,58 @@
+/**
+ * Ce fichier fait partie du projet flatcraft.
+ *
+ * (c) 2023 thibault.lombart
+ * Tous droits réservés.
+ */
+
+package fr.univartois.butinfo.r304.flatcraft.model.movables;
+
+import fr.univartois.butinfo.r304.flatcraft.model.FlatcraftGame;
+import fr.univartois.butinfo.r304.flatcraft.view.Sprite;
+import javafx.beans.property.IntegerProperty;
+
+/**
+ * Le type Mob
+ *
+ * @author thibault.lombart
+ *
+ * @version 0.1.0
+ */
+public class Mob extends AbstractMovable {
+
+    /**
+     * Crée une nouvelle instance de Mob.
+     * @param game
+     * @param xPosition
+     * @param yPosition
+     * @param sprite
+     */
+    private int healthPoints;
+    
+    private EMob type;
+    
+    private Sprite spriteDroit;
+    
+    private Sprite spriteGauche;
+    
+    public Mob(FlatcraftGame game, double xPosition, double yPosition, Sprite spriteDroit,Sprite spriteGauche, EMob type) {
+        // TODO Auto-generated constructor stub.
+        super(game, xPosition, yPosition, spriteDroit);
+        this.type = type;
+        this.spriteDroit = spriteDroit;
+        this.spriteGauche = spriteGauche;
+        this.healthPoints = type.getHealth();
+    }
+    
+    public int getHealthPoints() {
+        return healthPoints;
+    }
+
+    public void setHealthPoints(int healthPoints) {
+        this.healthPoints=healthPoints;
+    }
+    
+    
+
+}
+
